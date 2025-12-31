8d99bb2878f62d26b3962a24a6ae7a122515b633
babasiker826 <98yudistira@gmail.com>
2025-12-31 11:07:53 +0400

    Update backend4.py

--- backend4.py
+++ backend4.py
@@ -47,7 +47,7 @@ db = SQLAlchemy(app)
 limiter = Limiter(
     app=app,
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["1000 per day", "220 per hour"],
     storage_uri="memory://"
 )
 
